2275e985fcd5c077719bcd789c6b173bad9244b5
hitachiiii <mcmorzuk@mail.ru>
2020-12-05 00:08:16 +0300

    creating character controller
    + follow camera and animation of the hero

---
+++ GameProject/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float smoothing = 5f; //camera movement
+
+    Vector3 offset; //offset between the player and the camera
+
+    void Awake()
+    {
+        Assert.IsNotNull(target);
+    }
+
+    void Start()
+    {
+        offset = transform.position - target.position;
+    }
+
+    void Update()
+    {
+        Vector3 targetCameraPosition = target.position + offset; //the position that we want the camera to move to
+        transform.position = Vector3.Lerp(transform.position, targetCameraPosition, smoothing * Time.deltaTime); 
+    }
+}
---
+++ GameProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] private float moveSpeed = 10.0f;
+    private CharacterController characterController;
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        characterController.SimpleMove(moveDirection * moveSpeed);
+    }
+}
