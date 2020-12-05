a57ca20007fbf38e8fe35c0f68e7314b3789dfbf
hitachiiii <mcmorzuk@mail.ru>
2020-12-06 00:41:58 +0300

    enemy pathfinding & navigation

---
+++ GameProject/Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Assertions;
+
+public class EnemyMove : MonoBehaviour
+{
+    [SerializeField] Transform player;
+    private NavMeshAgent nav;
+    private Animator anim;
+
+    private void Awake()
+    {
+        Assert.IsNotNull(player);
+    }
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        nav.SetDestination(player.position);
+    }
+}
